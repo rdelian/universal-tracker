64d098e5e56f7a55acd0057b9851725f2a81c0e1
Raul N <darkyzzx@gmail.com>
2020-12-28 18:35:28 +0200

    update: create track

--- pages/create.js
+++ pages/create.js
@@ -1,74 +1,120 @@
 import Container from '@material-ui/core/Container';
-import { Checkbox } from '../components/Checkbox'
-
-const items = ['da/nu', 'number', 'text']
+import React from 'react';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
 
 export default function Create() {
-    const [tags, setTags] = React.useState([])
+    const [form, setForm] = React.useState({})
+    const [days, setDays] = React.useState([])
+    const curDate = new Date()
 
+    const handleChange = e => {
+        e.persist();
+        console.log(e.target.name, e.target.checked)
+        if (e.target.name == 'days') {
 
-    const handleTagChange = (tag, checked, multiple = false) => {
-        // if the same tag is clicked again, remove it 
-        if (tags.includes(tag)) {
-            const checkedTags = tags.filter(t => t !== tag)
-            console.log(checkedTags)
-            if (id === 'domeniu') {
-                setTags(checkedTags)
-                if (!checkedTags.length) {
-                    setQueried(false)
-                }
-                setQuery({
-                    [id]: checkedTags
-                })
-            } else {
-                setTags(checkedTags)
-                setQuery(values => ({
-                    ...values,
-                    [id]: checkedTags
-                }))
-            }
         } else {
-            // if the filter is suppossed to hold multiple clicked tags at once
-            if (multiple) {
-                const nextSelectedTags = checked ? [...tags, tag] : selectedOpTags.filter(t => t !== tag);
-                setTags(nextSelectedTags);
-                setQuery(values => ({
-                    ...values,
-                    [id]: nextSelectedTags
-                }))
-            } else {
-                // if the filter is holding only one tag
-                const nextSelectedTags = checked ? [tag] : selectedOpTags.filter(t => t !== tag);
-                setTags(nextSelectedTags);
-                setQuery(values => ({
-                    ...values,
-                    [id]: tag
-                }))
-            }
-            setQueried(true)
+            setForm(currentValues => ({
+                ...currentValues,
+                [e.target.name]: e.target.value
+            }))
         }
     }
+
+    const createTrack = async e => {
+        e.preventDefault()
+        const response = await fetch('api/createtrack', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(form) // body data type must match "Content-Type" header
+        });
+        console.log("sv response:", response.json())
+    }
+
+    console.log(form)
+
     return (
         <>
             <Container maxWidth="sm">
-                <h1> Add a track </h1>
-                <p>type of answer - checkbox</p>
-                {items.map(item => {
-                    return (
-                        <Checkbox key={item} type='checkbox'
-                            checked={tags.indexOf(item) > -1}
-                            tag={item}
-                            onChange={checked => handleTagChange(item, checked)}
-                        />
-                    )
-                })}
-                <p>whata do u want to track - input</p>
-                <p>frecventa</p>
-                <p>in ce zile ? - checkbox</p>
-                <p>de cate ori doriti pe zi? - checkbox</p>
-                <p>selecteaza ora - select</p>
-                <p> set finish date</p>
-                <button>create track</button>
+                <form onSubmit={createTrack}>
+                    <FormGroup>
+                        <h1> Add a track </h1>
+
+                        <span>
+                            <p>Type of answer</p>
+                            <RadioGroup aria-label="typeAnswerRadio" name="typeAnswerRadio">
+                                <span>
+                                    <FormControlLabel onChange={handleChange} name="typeAnswer" value="bool" control={<Radio />} label="Yes/No" />
+                                    <FormControlLabel onChange={handleChange} name="typeAnswer" value="number" control={<Radio />} label="Number" />
+                                    <FormControlLabel onChange={handleChange} name="typeAnswer" value="string" control={<Radio />} label="Text" />
+                                </span>
+                            </RadioGroup>
+                        </span>
+
+                        <span>
+                            <p>What do you want to track</p>
+                            <TextField id="outlined-basic" label="Track name" variant="outlined" onChange={handleChange} name="trackName" />
+                        </span>
+
+                        <span>
+                            <p>In which days</p>
+                            {["M", "T", "W", "T", "F", "S", "S"].map(
+                                (day, index) => <FormControlLabel
+                                    control={<Checkbox name={'days'} />}
+                                    label={day}
+                                    onChange={handleChange}
+                                    value={index}
+                                />
+                            )}
+                        </span>
+
+                        <span>
+                            <p>How many times per day</p>
+                            <RadioGroup aria-label="gender" name="gender1">
+                                <span>
+                                    <FormControlLabel value="female" control={<Radio />} label="1" />
+                                    <FormControlLabel value="male" control={<Radio />} label="2" />
+                                    <FormControlLabel value="other" control={<Radio />} label="3" />
+                                    <FormControlLabel value="other" control={<Radio />} label="Custom" />
+                                </span>
+                            </RadioGroup>
+                        </span>
+
+                        <span>
+                            <p>Select the hour</p>
+                            <TextField
+                                id="time"
+                                label=""
+                                type="time"
+                                defaultValue="07:30"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                        </span>
+
+                        <span>
+                            <p>Set finish date</p>
+                            <TextField
+                                id="date"
+                                label=""
+                                type="date"
+                                defaultValue={new Date(curDate.getFullYear() + 1, curDate.getMonth() + 1, 0).toISOString().slice(0, 10)}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                        </span>
+
+                        <button type="submit">create track</button>
+                    </FormGroup>
+                </form>
             </Container>
         </>
     )
