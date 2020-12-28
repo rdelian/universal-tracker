f791ca96f86fddf2fbc0624400e8f9c31173081d
Raul N <darkyzzx@gmail.com>
2020-12-28 15:19:17 +0200

    add: development option

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
+  devIndicators: {
+    autoPrerender: false,
+  },
   pwa: {
     dest: 'public',
-    runtimeCaching,
+    // runtimeCaching,
   },
 })
