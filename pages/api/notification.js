323f34ccc885b4d2f352e620a8aadd15412e3ce7
Raul N <darkyzzx@gmail.com>
2021-01-02 19:07:48 +0200

    add: web-push

---
+++ pages/api/notification.js
@@ -0,0 +1,31 @@
+import { RssFeedTwoTone } from '@material-ui/icons'
+
+const webPush = require('web-push')
+
+webPush.setVapidDetails(
+    `mailto:${process.env.WEB_PUSH_EMAIL}`,
+    process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY,
+    process.env.WEB_PUSH_PRIVATE_KEY
+)
+
+export default (req, res) => {
+    if (req.method == 'POST') {
+        const { subscription } = req.body
+
+        webPush
+            .sendNotification(subscription, JSON.stringify({ title: 'Universal Tracker', message: 'Cate flotari ai bagat azi bosultane?' }))
+            .then(response => {
+                res.writeHead(response.statusCode, response.headers).end(response.body)
+            })
+            .catch(err => {
+                if ('statusCode' in err) {
+                    res.writeHead(err.statusCode, err.headers).end(err.body)
+                } else {
+                    console.error(err)
+                    res.status(500).end()
+                }
+            })
+    } else {
+        res.status(405).end()
+    }
+}
